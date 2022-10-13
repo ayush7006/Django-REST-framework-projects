c1882d0b240b732fd04829d273d9077f3136856f
ayush7006 <ayush123gurjar.com>
2022-10-13 11:22:52 +0530

    fanction view into class view

--- CRUD_DRF_api_view/CRUD_DRF_api_view/urls.py
+++ CRUD_DRF_api_view/CRUD_DRF_api_view/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Studentapi/', views.Student_api),
-    path('Studentapi/<int:pk>', views.Student_api),
+    path('Studentapi/', views.StudentAPI.as_view()),#path('Studentapi/', views.Student_api),
+    path('Studentapi/<int:pk>', views.StudentAPI.as_view()),#path('Studentapi/<int:pk>', views.Student_api),
 ]
--- CRUD_DRF_api_view/api/views.py
+++ CRUD_DRF_api_view/api/views.py
@@ -1,11 +1,53 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Student
 from .serializers import StudentSerializer
 
+#class based view
 
-# Create your views here.
+from rest_framework.views import APIView  
+class StudentAPI(APIView):
+    def get(self,request,pk=None,format=None):
+        id = pk                                  #id = request.data.get('id')
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            return Response(serializer.data)
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu , many=True)
+        return Response(serializer.data)
+    def post(self,request,format=None):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'data created'})
+        return Response(serializer.errors)
+    def put(self,request,pk=None,format=None):
+        id = pk                                  #id = request.data.get('id')
+        stu = Student.objects.get(pk=id) 
+        serializer = StudentSerializer(stu ,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':'complit data updated'})
+        return Response(serializer.errors)
+    def patch(self,request,pk=None,format=None):
+        id = pk                                  #id = request.data.get('id')
+        stu = Student.objects.get(pk=id)
+        serializer = StudentSerializer(stu ,data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':' PATCH data updated'})
+        return Response(serializer.errors)
+    def delete(self,request,pk=None,format=None):
+        id = pk                                  #id = request.data.get('id') 
+        stu = Student.objects.get(pk=id)
+        stu.delete()
+        return Response({'msg':'data deleted'})
+
+
+# function baesd view
+#from rest_framework.decorators import api_view
+'''
 @api_view(['GET','POST','PUT','PATCH','DELETE'])   #patch added
 def Student_api(request,pk=None):
     if request.method == 'GET':
@@ -52,3 +94,4 @@ def Student_api(request,pk=None):
         stu = Student.objects.get(pk=id)
         stu.delete()
         return Response({'msg':'data deleted'}) 
+'''
