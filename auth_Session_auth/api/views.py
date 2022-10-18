c03bc30deb6029e8f5be6de1856ff6ff8692c572
ayush7006 <ayush123gurjar.com>
2022-10-18 11:19:44 +0530

    throttling added

--- auth_Session_auth/api/views.py
+++ auth_Session_auth/api/views.py
@@ -4,18 +4,22 @@ from .serializers import StudentSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly, DjangoModelPermissions
-
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 
 
 class StudentViewset(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
+
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
+    #throttle_scope = ''
+
     authentication_classes = [SessionAuthentication]
     #permission_classes = [IsAuthenticated]
     #permission_classes = [IsAdminUser]
-    #permission_classes = [IsAuthenticatedOrReadOnly]
-    permission_classes = [DjangoModelPermissions]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    #permission_classes = [DjangoModelPermissions]
     #permission_classes = [IsAuthenticatedOrReadOnly]
 
 # HOW TO USE FUNCTON AUTH
--- auth_Session_auth/auth_Session_auth/settings.py
+++ auth_Session_auth/auth_Session_auth/settings.py
@@ -123,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FARMEWORK = {
+    'DEFAULT_THROTTALE_RATE':{
+        'anon':'2/day',
+        'user':'5/hour'
+    }
+}
