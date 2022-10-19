3fc1d32c89cfcdcdb26fc6db775b1a716b2236f5
ayush7006 <ayush123gurjar.com>
2022-10-19 15:46:01 +0530

    page_no_pagination

---
+++ page_no_pagination/api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer 
+from rest_framework.generics import ListAPIView
+
+
+# Create your views here.
+
+class StudentList(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
